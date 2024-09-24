7125c53ad2ccf1313aea73f759d88d5e371323d2
gabriel1003 <gabriel.samersla97@gmail.com>
2024-09-24 09:18:23 -0300

    Adicionando os loops e decisoes

---
+++ condicao2.dart
@@ -0,0 +1,32 @@
+// criando um switch case
+
+void main() {
+
+  String diaDaSemana = "domingo";
+
+  switch (diaDaSemana) {
+    case "domingo":
+    print("é final de semana! ");
+    break;
+    case "segunda":
+    print("Começando a semana! ");
+    break;
+    case "terça":
+    print("quase no meio da semana! ");
+    break;
+    case "quarta":
+    print("Chegou o meio da semana! ");
+    break;
+    case "quinta":
+    print("Chegando o final da semana! ");
+    break;
+    case "sexta":
+    print("Semana acabo! ");
+    break;
+    case "sabado":
+    print("Finalmente descanso! ");
+    break;
+default:
+print("digite um dia válido! ");
+  }
+}
